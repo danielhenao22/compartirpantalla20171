3803417ea65c353cad20ea0e01c14278f8705d9d
danie <danie@DESKTOP-92V359R>
2017-02-23 13:12:10 -0500

    commit 3

---
+++ compartirpantalla-servidor/src/main/java/co/edu/eam/dao/SingletonEntityManager.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.eam.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author danie
+ */
+class SingletonEntityManager {
+
+    private static EntityManager em;
+
+    private SingletonEntityManager() {
+        em = Persistence.createEntityManagerFactory("servidorTCPPU").createEntityManager();
+    }
+
+    public static EntityManager getInstance() {
+        if (em == null) {
+            new SingletonEntityManager();
+        }
+        return em;
+    }
+}
---
+++ compartirpantalla-servidor/src/main/java/co/edu/eam/dao/UsuarioJpaController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.eam.dao;
+
+import co.edu.eam.modelo.Usuario;
+import java.io.Serializable;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author danie
+ */
+public class UsuarioJpaController implements Serializable {
+    
+    protected EntityManager em;
+
+    public UsuarioJpaController() {
+        this.em = SingletonEntityManager.getInstance();
+    }
+
+
+    public void create(Usuario usuario) {
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
+    }
+
+    public void edit(Usuario usuario) {
+        em.getTransaction().begin();
+        em.merge(usuario);
+        em.getTransaction().commit();
+    }
+
+    public void destroy(Usuario usuario) {
+        em.getTransaction().begin();
+        em.remove(usuario);
+        em.getTransaction().commit();
+    }
+
+    public Usuario findUsuario(Integer id) {
+        return em.find(Usuario.class, id);
+    }
+
+}
